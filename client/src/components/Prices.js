060777970c262d43078cf74b9cdd02454cd6aea6
Kratika <kratikamangal11@gmail.com>
2023-11-22 19:00:22 +0530

    first commit

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "₹200 to ₹500",
+      array: [0, 19],
+    },
+    {
+      _id: 1,
+      name: "₹500 to ₹1000",
+      array: [20, 39],
+    },
+    {
+      _id: 2,
+      name: "₹1000 to ₹1500 ",
+      array: [40, 59],
+    },
+    {
+      _id: 3,
+      name: "₹1500 to ₹2000",
+      array: [60, 79],
+    },
+    {
+      _id: 4,
+      name: "₹2000 to ₹3000",
+      array: [80, 99],
+    },
+    {
+      _id: 4,
+      name: "₹3000 or more",
+      array: [100, 9999],
+    },
+  ];
